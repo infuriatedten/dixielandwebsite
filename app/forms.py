9ba2f9a4445429dda0501a2df57a618d770cb9a0
infuriatedten <79727002+infuriatedten@users.noreply.github.com>
2025-07-26 16:24:48 -0400

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -7,7 +7,23 @@ from wtforms.validators import DataRequired, Email, Length, NumberRange, Optiona
 from wtforms.fields import FloatField
 
 # ------------- User Management ------------- #
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo
 
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Log In')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+    
 class EditUserForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=3, max=64)])
     email = StringField('Email', validators=[DataRequired(), Email(), Length(max=120)])
